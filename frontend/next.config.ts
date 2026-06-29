b3a99e483f388abd8e3dd8f85f5e82ca93a514a0
FelipeHeringer <heringerfelipe14@gmail.com>
2026-06-29 00:39:55 -0300

    chore: update next.config file by adding cloudinary permition

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "http", hostname: "localhost", port: "8080" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+    ],
+  },
+};
 
 export default nextConfig;
